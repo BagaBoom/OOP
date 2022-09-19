cc5e55604b9092e7cdb5794eacd7b9450218ae1a
BagaBoom <111866879+BagaBoom@users.noreply.github.com>
2022-09-19 09:07:13 +0300

    Add files via upload

---
+++ OPP lab 4.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <locale.h>
+using namespace std;
+
+class MyClass {
+private:
+    float* p;
+    int size;
+public:
+    MyClass(int size) {
+        this->size = size;
+        this->p = new float[size];
+        for (int i = 0; i < int(size); i++) {
+            if ((i % 2) == 0) {
+                p[i] = i - 1.9;
+            }
+            else {
+                p[i] = i;
+            }
+        }
+        cout << "Визвався конструктор " << this << endl;
+        for (int i = 0; i < int(size); i++) {
+            cout << p[i] << " ";
+        }
+        cout << endl;
+    }
+
+    MyClass(const MyClass& other) {
+        this->size = other.size;
+        this->p = new float[other.size];
+        int k = 0;
+        float c,f =0;
+        cout << "Визвався конструктор копіювання " << this << endl;
+        for (int i = other.size-1; i > 0; i--) {
+            this->p[i] = other.p[i];
+            cout << p[i] << " ";
+        }
+        cout << endl;
+
+       
+
+    }
+
+    ~MyClass() {
+        cout << "Визвався деструктор " << this << endl;
+        delete[] p;
+    }
+
+};
+
+int main() {
+
+    setlocale(0, "UKRAINIAN");
+    MyClass x(14);
+    MyClass y(x);
+    system("pause");
+    return 0;
+}
